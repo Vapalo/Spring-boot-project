5f17c24193f9481c04b70ddc3b5a4af79ab1e244
Toni Vapalo <toni.vapalo@gmail.com>
2021-03-19 15:26:37 +0200

    Initial commit

---
+++ AlbumList/src/main/java/com/POkevat/AlbumList/AlbumListApplication.java
@@ -0,0 +1,13 @@
+package com.POkevat.AlbumList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumListApplication.class, args);
+	}
+
+}
---
+++ AlbumList/src/test/java/com/POkevat/AlbumList/AlbumListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POkevat.AlbumList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
